263e64adddb25b6c72919aabef4cf3a28ce7475d
Maria Jose Gonzalez <mjgonzalez6@miuandes.cl>
2018-06-09 14:09:49 -0400

    Elementos para crear el catálogo añadidos

--- app/src/main/java/com/example/lenovo/proyectoaplicacionesmoviles/Catalogo/CatalogoVistaFragment.java
+++ app/src/main/java/com/example/lenovo/proyectoaplicacionesmoviles/Catalogo/CatalogoVistaFragment.java
@@ -5,6 +5,36 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.database.Cursor;
+import android.database.MergeCursor;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 
 import com.example.lenovo.proyectoaplicacionesmoviles.R;
 
@@ -13,6 +43,10 @@ import com.example.lenovo.proyectoaplicacionesmoviles.R;
  */
 
 public class CatalogoVistaFragment extends Fragment {
+
+
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
